e3270a50aa75b1e046544ac2300a1efd6331e758
suresh Kumar <sureshkumar.medasani@gmail.com>
2019-10-18 16:12:02 +0530

    PayPal capture id

--- app/src/main/java/com/example/smedasani/ec/MainActivity.java
+++ app/src/main/java/com/example/smedasani/ec/MainActivity.java
@@ -1,39 +1,56 @@
 package com.example.smedasani.ec;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.braintreepayments.api.BraintreeFragment;
 import com.braintreepayments.api.PayPal;
+import com.braintreepayments.api.exceptions.BraintreeError;
+import com.braintreepayments.api.exceptions.ErrorWithResponse;
 import com.braintreepayments.api.exceptions.InvalidArgumentException;
+import com.braintreepayments.api.interfaces.BraintreeCancelListener;
+import com.braintreepayments.api.interfaces.BraintreeErrorListener;
 import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
 import com.braintreepayments.api.models.PayPalAccountNonce;
 import com.braintreepayments.api.models.PayPalRequest;
 import com.braintreepayments.api.models.PaymentMethodNonce;
 import com.braintreepayments.api.models.PostalAddress;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
 
-import cz.msebera.android.httpclient.Header;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements PaymentMethodNonceCreatedListener {
+import cz.msebera.android.httpclient.Header;
+// Braintree link https://developers.braintreepayments.com/guides/client-sdk/setup/android/v2#browser-switch-setup
+public class MainActivity extends AppCompatActivity implements PaymentMethodNonceCreatedListener,BraintreeCancelListener,BraintreeErrorListener {
 
     private String clientToken="";
     private String nonce;
     private BraintreeFragment mBraintreeFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("Suresh******  ","onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Log.d("Suresh******  exit  ","onCreate");
+    }
+    public void payNow(View v){
+        Log.d("Suresh******  enter  ","payNow");
+        final Activity activity=this;
+
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("https://ecbtserver.cfapps.io/client_token", new TextHttpResponseHandler() {
+        client.get("https://ppbraintree.herokuapp.com/client_token_sb", new TextHttpResponseHandler() {
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
             }
@@ -41,25 +58,22 @@ public class MainActivity extends AppCompatActivity implements PaymentMethodNonc
             public void onSuccess(int statusCode, Header[] headers, String response) {
                 Log.i("Suresh******Token",response);
                 clientToken = response;
+                try {
+                    mBraintreeFragment = BraintreeFragment.newInstance(activity, clientToken);
+                    Log.i("Suresh******  mBrain", "done");
+                } catch (InvalidArgumentException e) {
+                    // There was an issue with your authorization string.
+                    Log.e("Suresh******  mBrain",e.toString());
+
+                }
+                setupBraintreeAndStartExpressCheckout();
+                // setupBraintreeAndStartExpressCheckout();
+                final TextView tvOut = (TextView) findViewById(R.id.textView);
+              //  tvOut.setText("pay success");
 
             }
         });
-        Log.d("Suresh******  exit  ","onCreate");
-    }
-    public void payNow(View v){
-        Log.d("Suresh******  enter  ","payNow");
-        try {
-            mBraintreeFragment = BraintreeFragment.newInstance(this, clientToken);
-            Log.i("Suresh******  mBrain", "done");
-        } catch (InvalidArgumentException e) {
-            // There was an issue with your authorization string.
-            Log.e("Suresh******  mBrain",e.toString());
 
-        }
-        setupBraintreeAndStartExpressCheckout();
-       // setupBraintreeAndStartExpressCheckout();
-        final TextView tvOut = (TextView) findViewById(R.id.textView);
-        tvOut.setText("pay success");
         Log.d("Suresh******  exit  ","payNow");
     }
 
@@ -95,15 +109,22 @@ public class MainActivity extends AppCompatActivity implements PaymentMethodNonc
         RequestParams params = new RequestParams();
      /*   params.setForceMultipartEntityContentType(true);
         params.setContentEncoding("application/x-www-form-urlencoded");*/
-        params.put("payment_method_nonce", nonce);
+        params.put("nonce", nonce);
       //  params.put("amount1", "20");
         Log.d("nonce  ",nonce);
-        client.post("https://ecbtserver.cfapps.io/checkout", params,
+       // client.post("https://ecbtserver.cfapps.io/checkout", params,
+        client.post("https://ppbraintree.herokuapp.com/checkout_sb", params,
                 new AsyncHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                         String body = new String(responseBody);
+
                         Log.d("payment  ",body);
+                        final TextView tvOut = (TextView) findViewById(R.id.textView);
+
+                            tvOut.setText(body);
+
+
                     }
 
                     @Override
@@ -118,12 +139,36 @@ public class MainActivity extends AppCompatActivity implements PaymentMethodNonc
 
     private void setupBraintreeAndStartExpressCheckout() {
         Log.d("Suresh******  enter  ","setupBraintreeAndStartExpressCheckout");
-        PayPalRequest request = new PayPalRequest("5")
+        PayPalRequest request = new PayPalRequest("10")
                 .currencyCode("USD")
                 .intent(PayPalRequest.INTENT_SALE);
 
         PayPal.requestOneTimePayment(mBraintreeFragment, request);
         Log.d("Suresh******  exit  ","setupBraintreeAndStartExpressCheckout");
+    }
+
+    @Override
+    public void onError(Exception error) {
+        if (error instanceof ErrorWithResponse) {
+            ErrorWithResponse errorWithResponse = (ErrorWithResponse) error;
+            BraintreeError cardErrors = errorWithResponse.errorFor("creditCard");
+            if (cardErrors != null) {
+                // There is an issue with the credit card.
+                BraintreeError expirationMonthError = cardErrors.errorFor("expirationMonth");
+                if (expirationMonthError != null) {
+                    // There is an issue with the expiration month.
+                    Log.d("Suresh******  exit  ","setupBraintreeAndStartExpressCheckout");
+                    Toast.makeText(this,expirationMonthError.getMessage(),Toast.LENGTH_LONG);
+                   //setErrorMessage(expirationMonthError.getMessage());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onCancel(int requestCode) {
+        // Use this to handle a canceled activity, if the given requestCode is important.
+        // You may want to use this callback to hide loading indicators, and prepare your UI for input
     }
 
 }
